d458f26991ead874efd1371ef1f18d60fe0907ec
JJH <jakebbak@gmail.com>
2023-05-17 19:50:23 +0900

    2023/05/17: 테스트

---
+++ src/test/java/SpringMVC/productservice3/Productservice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringMVC.productservice3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productservice3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
