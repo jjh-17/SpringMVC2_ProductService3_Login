c78777fe602d61b1d3ffc0e7d417fe5c5524e66a
JJH <jakebbak@gmail.com>
2023-05-17 19:53:36 +0900

    2023/05/17: 도메인 - 회원

---
+++ src/main/java/SpringMVC/productservice3/domain/member/Member.java
@@ -0,0 +1,27 @@
+package SpringMVC.productservice3.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Member {
+
+    private Long memberId;
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String password;
+
+    public Member(String name, String loginId, String password) {
+        this.name = name;
+        this.loginId = loginId;
+        this.password = password;
+    }
+}
