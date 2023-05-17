2b4d16e5cb5246694d4f8623132d0e68bc005364
JJH <jakebbak@gmail.com>
2023-05-17 19:56:53 +0900

    2023/05/17: 로그인 폼

---
+++ src/main/java/SpringMVC/productservice3/web/login/LoginForm.java
@@ -0,0 +1,14 @@
+package SpringMVC.productservice3.web.login;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+}
