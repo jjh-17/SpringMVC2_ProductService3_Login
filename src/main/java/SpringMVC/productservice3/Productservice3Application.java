b944bbdbe9bba855c96228211de0269a71dc9595
JJH <jakebbak@gmail.com>
2023-05-17 19:59:15 +0900

    2023/05/17: 상품 서비스 시작

---
+++ src/main/java/SpringMVC/productservice3/Productservice3Application.java
@@ -0,0 +1,13 @@
+package SpringMVC.productservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productservice3Application.class, args);
+	}
+
+}
