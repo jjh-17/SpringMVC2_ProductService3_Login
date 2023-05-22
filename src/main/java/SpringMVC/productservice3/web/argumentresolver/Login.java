ae4b17249c50b1bbd05a8d846b5c6b7e4b235c8b
JJH <jakebbak@gmail.com>
2023-05-22 21:04:58 +0900

    2023/05/18: 사용자 설정 로그인 어노테이션

---
+++ src/main/java/SpringMVC/productservice3/web/argumentresolver/Login.java
@@ -0,0 +1,11 @@
+package SpringMVC.productservice3.web.argumentresolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) //파라미터에만 사용한다.
+@Retention(RetentionPolicy.RUNTIME) //런타임까지 애노테이션 정보가 남아있는다 ==> 리플렉션 등의 활용 가능
+public @interface Login {
+}
